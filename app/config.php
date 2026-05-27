cf34d88f85c68c94e021e0e8c388b63176c25c94
Crystian <crystian@gmail.com>
2026-05-27 23:46:20 +0200

    env

--- app/config.php
+++ app/config.php
@@ -1,9 +1,7 @@
-<?php 
-define('DB_SERVER', 'mysql.railway.internal:3306');
-define('DB_USER', 'root');
-define('DB_PASSWD', '');
-define('DATABASE', 'filmtoread');
+<?php
+define('DB_SERVER', getenv('DB_SERVER') ?: 'localhost');
+define('DB_USER', getenv('DB_USER') ?: 'root');
+define('DB_PASSWD', getenv('DB_PASSWD') ?: '');
+define('DATABASE', getenv('DATABASE') ?: 'filmtoread');
 define('SESSION_TIMEOUT', 600); // 10 minutos
-
-
 ?>
