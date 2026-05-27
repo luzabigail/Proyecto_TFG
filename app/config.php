482c8e4bfeef8c0f36dfb16590809404a955469c
Crystian <crystian@users.noreply.github.com>
2026-05-27 23:34:43 +0200

    Update database connection details in config.php

--- app/config.php
+++ app/config.php
@@ -1,8 +1,8 @@
 <?php 
-define('DB_SERVER', 'localhost');
+define('DB_SERVER', 'mysql.railway.internal:3306');
 define('DB_USER', 'root');
-define('DB_PASSWD', '');
-define('DATABASE', 'FilmtoRead');
+define('DB_PASSWD', 'fdijuROArSrdGysMeuuemOBwQhQKhbfu');
+define('DATABASE', 'filmtoread');
 define('SESSION_TIMEOUT', 600); // 10 minutos
 
 
