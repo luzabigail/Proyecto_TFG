049d277f9557bb5bc4e19e15d71b4fd8f0f3037f
Crystian <crystian@users.noreply.github.com>
2026-05-27 23:37:41 +0200

    Update config.php

--- app/config.php
+++ app/config.php
@@ -1,7 +1,7 @@
 <?php 
 define('DB_SERVER', 'mysql.railway.internal:3306');
 define('DB_USER', 'root');
-define('DB_PASSWD', 'fdijuROArSrdGysMeuuemOBwQhQKhbfu');
+define('DB_PASSWD', '');
 define('DATABASE', 'filmtoread');
 define('SESSION_TIMEOUT', 600); // 10 minutos
 
